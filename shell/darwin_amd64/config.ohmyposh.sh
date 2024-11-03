dddd34c89d1e6b1718af6cce3fd431446635fa8a
Jaouad CHAKIR <jchakir@student.1337.ma>
2024-11-03 09:09:30 +0000

    fix(shell.config.ohmyposh): small fix in order of commands

--- shell/darwin_amd64/config.ohmyposh.sh
+++ shell/darwin_amd64/config.ohmyposh.sh
@@ -8,8 +8,8 @@ _set_prereq 'curl' 'curl'
 
 # install and setup ohmyposh
 _setup_ohmyposh() {
-  curl -s https://ohmyposh.dev/install.sh | bash -s -- -d "$BIN_DIR" && \
   mv -f ~/.config/ohmyposh ~/.config/ohmyposh.bak 2> /dev/null
+  curl -s https://ohmyposh.dev/install.sh | bash -s -- -d "$BIN_DIR" && \
   ln -sfn "$(pwd)/HOME/config/ohmyposh" ~/.config/ohmyposh 2> /dev/null
   [[ $? -ne 0 ]] && _error "ohmyposh: Erorr while setuping 'ohmyposh'"
   _info "ohmyposh: Successfully instelled"
--- shell/linux_amd64/config.ohmyposh.sh
+++ shell/linux_amd64/config.ohmyposh.sh
@@ -7,8 +7,8 @@ PRE_REQUISITES['curl']='curl'
 
 # install and setup ohmyposh
 _setup_ohmyposh() {
-  curl -s https://ohmyposh.dev/install.sh | bash -s -- -d "$BIN_DIR" && \
   mv ~/.config/ohmyposh ~/.config/ohmyposh.bak
+  curl -s https://ohmyposh.dev/install.sh | bash -s -- -d "$BIN_DIR" && \
   ln -sfn "$(pwd)/HOME/config/ohmyposh" ~/.config/ohmyposh
   [[ $? -ne 0 ]] && _error "ohmyposh: Erorr while setuping 'ohmyposh'"
   _info "ohmyposh: Successfully instelled"
