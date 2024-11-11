4b7455cd1549108ae15454a02c4f49cff805ad3f
Jaouad Chakir <jchakir@c4r6p5.1337.ma>
2024-11-11 15:04:08 +0100

    change(config.nvim): change colorscheme to onedark_pro

--- HOME/config/nvim/lua/config/lazy.lua
+++ HOME/config/nvim/lua/config/lazy.lua
@@ -30,7 +30,7 @@ require("lazy").setup({
 		version = false, -- always use the latest git commit
 		-- version = "*", -- try installing the latest stable version for plugins that support semver
 	},
-	install = { colorscheme = { "onedark" } }, -- Define Your Color Scheme Here
+	install = { colorscheme = { "onedark_dark" } }, -- Define Your Color Scheme Here
 	checker = {
 		enabled = true, -- check for plugin updates periodically
 		notify = false, -- notify on update
