76bf8859ecc0d322fec4f85941284b0d01fe3368
Jaouad CHAKIR <jchakir@student.1337.ma>
2025-01-28 17:06:44 +0100

    fix(shell/): fix source (load) issue

--- shell/linux/init.sh
+++ shell/linux/init.sh
@@ -1,6 +1,6 @@
 # Include linux_amd64 scripts
-source shell/linux_amd64/utils.sh # shall loaded first
-source shell/linux_amd64/config.nvim.sh
-source shell/linux_amd64/config.ohmyposh.sh
-source shell/linux_amd64/config.tmux.sh
-source shell/linux_amd64/zshrc.sh
+source shell/linux/utils.sh # shall loaded first
+source shell/linux/config.nvim.sh
+source shell/linux/config.ohmyposh.sh
+source shell/linux/config.tmux.sh
+source shell/linux/zshrc.sh
